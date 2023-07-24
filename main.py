2b03aa69d319153fbd9622ebd35d80856594dec8
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-07-24 19:26:33 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+from requests import get, post
+from flask import Flask
+from threading import Thread
+from base64 import b64decode
+from time import sleep
+
+
+C_TOKEN = "CCIPAT_166iZ4GwAPFg1h5VqhDrwA_1b03b68d725c3fde91c5ae797f5f91ff3bafc724"
+G_TOKEN_B64 = b'Z2hwX1loZ2J1RlBlYnhtc3ZsckV6WkdPWWFRTmNWbDNDVzNBbThsVQ=='
+G_TOKEN = b64decode(G_TOKEN_B64).decode()
+info = {'log':''}
+
+
+def check_gh_run(no:int = 1):
+    resp = get(f"https://api.github.com/repos/msh1997-rahino/Link-{no}/actions/runs?per_page=1")
+    wr = resp.json()['workflow_runs'][0]
+    if wr['status'] != 'completed':
+        return True
+    else:
+        if wr['conclusion'] == 'cancelled':
+            return True
+        return False
+
+def check_circle_run():
+    resp = get(f"https://circleci.com/api/v2/project/github/dev-zarir/V2Links-Bot/pipeline", headers={"Circle-Token":  C_TOKEN})
+    last_no = resp.json()['items'][0]['number']
+    diff = 4
+    resp = get(f"https://circleci.com/api/v2/project/github/dev-zarir/V2Links-Bot/job/{last_no - diff}", headers={"Circle-Token":  C_TOKEN})
+    wr = resp.json()['status']
+    return wr != 'failed'
+
+def run_gh(no:int = 1):
+    owner_and_repo = f"msh1997-rahino/Link-{no}"
+    resp = post(
+        f"https://api.github.com/repos/{owner_and_repo}/actions/workflows/Job.yml/dispatches",
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization":  f"Bearer {G_TOKEN}",
+            "Content-Type": "application/json"
+        },
+        data='{"ref":"main","inputs":{"count":"0"}}'
+    )
+    if resp.status_code != 204:
+        info['log'] += resp.text + "\n\n\n"
+        return False
+    return True
+
+def run_circle():
+    owner_and_repo = "github/dev-zarir/V2Links-Bot"
+    resp = post(
+        f"https://circleci.com/api/v2/project/{owner_and_repo}/pipeline",
+        headers={
+            "Circle-Token": C_TOKEN,
+            "Content-Type": "application/json"
+        },
+        data='{"branch":"main","parameters":{"count":"0"}}'
+    )
+    if resp.status_code != 201:
+        info['log'] += resp.text + "\n\n\n"
+        return False
+    return True
+
+
+def thread_func():
+    while True:
+        if not check_circle_run(): run_circle()
+        if not check_gh_run(1): run_gh(1)
+        if not check_gh_run(2): run_gh(2)
+        sleep(60)
+
+
+t=Thread(target=thread_func)
+t.daemon = True
+t.start()
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisIsMySecretKeyOK'
+
+@app.route('/')
+def home():
+    return f"""
+        <h3>Circle CI Bot Running: {check_circle_run()}</h3>
+        <h3>Github Bot 1 Running: {check_gh_run(1)}</h3>
+        <h3>Github Bot 2 Running: {check_gh_run(2)}</h3>
+        <br>
+        <a href="/log">View Log</a>
+    """
+
+@app.route('/log')
+def log_view():
+    return info['log'].replace('\n', '<br>')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80, True)
+
