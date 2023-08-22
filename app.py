48deae1dc6e66047220422a245c71f4133490e97
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-08-22 22:01:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from time import sleep, time
 import traceback
 
 
-all_tokens = {
+all_ci_tokens = {
     'dev-zarir' : 'CCIPAT_166iZ4GwAPFg1h5VqhDrwA_1b03b68d725c3fde91c5ae797f5f91ff3bafc724',
     'makhatun204' : 'CCIPAT_CDqH8Y8sCHan79JyX2Htsn_8cdb7ae29215ed1b98a8d68b99b0fad941db21bd',
     'jskpg2019' : 'CCIPAT_5dN99379pRkoGgPZGeebEk_5be704f48cef525073f773a9c355d740ae581e6d'
 }
+all_gh_tokens = {
+    'jskpg2019': get('https://pastebin.com/raw/8EC9XUc0').text,
+}
 
-G_TOKEN = get('https://pastebin.com/raw/8EC9XUc0').text
-G_USER = 'jskpg2019'
-info = {'log':'','last_check': time(),'c1':False,'c2':False,'c3':False, 'g1':False}
+info = {'log':'','last_check': time()}
 
-
-def check_gh_run(no:int = 1):
+def check_gh_run(usr):
     try:
-        resp = get(f"https://api.github.com/repos/{G_USER}/Links-Bot/actions/runs?per_page=1", headers={"Authorization": f"Bearer {G_TOKEN}"})
+        resp = get(f"https://api.github.com/repos/{usr}/Links-Bot/actions/runs?per_page=1", headers={"Authorization": f"Bearer {all_gh_tokens.get(usr)}"})
         try:
             wr = resp.json()['workflow_runs'][0]
         except:
@@ -36,11 +36,11 @@ def check_gh_run(no:int = 1):
 
 def check_circle_run(usr):
     try:
-        resp = get(f"https://circleci.com/api/v2/project/github/{usr}/Links-Bot/pipeline", headers={"Circle-Token":  all_tokens.get(usr)})
+        resp = get(f"https://circleci.com/api/v2/project/github/{usr}/Links-Bot/pipeline", headers={"Circle-Token":  all_ci_tokens.get(usr)})
         last_no = resp.json()['items'][0]['number']
         for diff in range(0,16):
             try:
-                resp = get(f"https://circleci.com/api/v2/project/github/{usr}/Links-Bot/job/{last_no - diff}", headers={"Circle-Token": all_tokens.get(usr)})
+                resp = get(f"https://circleci.com/api/v2/project/github/{usr}/Links-Bot/job/{last_no - diff}", headers={"Circle-Token": all_ci_tokens.get(usr)})
                 wr = resp.json()['status']
                 return wr != 'failed'
             except KeyError:
@@ -49,14 +49,13 @@ def check_circle_run(usr):
         info['log']+=traceback.format_exc() + '\n\n\n'
         return None
 
-def run_gh(no:int = 1):
+def run_gh(usr):
     try:
-        owner_and_repo = f"{G_USER}/Links-Bot"
         resp = post(
-            f"https://api.github.com/repos/{owner_and_repo}/actions/workflows/Job.yml/dispatches",
+            f"https://api.github.com/repos/{usr}/Links-Bot/actions/workflows/Job.yml/dispatches",
             headers={
                 "Accept": "application/vnd.github+json",
-                "Authorization":  f"Bearer {G_TOKEN}",
+                "Authorization":  f"Bearer {all_gh_tokens.get(usr)}",
                 "Content-Type": "application/json"
             },
             data='{"ref":"main","inputs":{"count":"0"}}'
@@ -74,7 +73,7 @@ def run_circle(usr):
         resp = post(
             f"https://circleci.com/api/v2/project/github/{usr}/Links-Bot/pipeline",
             headers={
-                "Circle-Token": all_tokens.get(usr),
+                "Circle-Token": all_ci_tokens.get(usr),
                 "Content-Type": "application/json"
             },
             data='{"branch":"main","parameters":{"count":"0"}}'
@@ -103,19 +102,28 @@ def thread_func():
     while True:
         try:
             i=1
-            for usr in list(all_tokens):
+            for usr in list(all_ci_tokens):
                 info[f'c{i}'] = check_circle_run(usr)
                 i+=1
-            sleep(10)
             i=1
-            for usr in list(all_tokens):
+            for usr in list(all_gh_tokens):
+                info[f'g{i}'] = check_gh_run(usr)
+                i+=1
+                
+            sleep(10)
+            
+            i=1
+            for usr in list(all_ci_tokens):
                 if not info[f'c{i}']:
                     if not check_circle_run(usr):
                         info[f'c{i}']=run_circle(usr)
                 i+=1
-            info['g1'] = check_gh_run()
-            if info['g1'] == False:
-                info['g1']=run_gh()
+            i=1
+            for usr in list(all_gh_tokens):
+                if not info[f'g{i}']:
+                    if not check_gh_run(usr):
+                        info[f'g{i}']=run_gh(usr)
+                i+=1
             info['last_check']=time()
         except:
             info['log']+=traceback.format_exc() + '\n\n\n'
@@ -133,11 +141,15 @@ tag_meta = '<meta name="viewport" content="width=device-width, initial-scale=1.0
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    content=''
+    for key, value in info.items():
+        if 'l' not in key:
+            if 'c' in key:
+                content+=f'<h3>Circle CI Bot {key.replace("c","")} Running: {value}</h3>'
+            if 'g' in key:
+                content+=f'<h3>Github Bot {key.replace("g","")} Running: {value}</h3>'
     return f"""{tag_meta}
-        <h3>Circle CI Bot 1 Running: {info['c1']}</h3>
-        <h3>Circle CI Bot 2 Running: {info['c2']}</h3>
-        <h3>Circle CI Bot 3 Running: {info['c3']}</h3>
-        <h3>Github Bot 1 Running: {info['g1']}</h3>
+        {content}
         <p><span style="color:red;">Note:</span> If the CI Bot is manually cancelled/stopped, then it will be count as running. Only failed CI/Bot will set as running = False.</p>
         <br>
         <h3>Last Checked: {seconds_to_time(time() - info['last_check'])} ago</h3>
